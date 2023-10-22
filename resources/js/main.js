766b9dffd33307d1858d37d07c312cd4d8bf3788
kaderabdul79 <kaderabdul7980@gmail.com>
2023-10-22 06:19:51 +0600

    setup vue,vuetify,router,vform

---
+++ resources/js/main.js
@@ -0,0 +1,29 @@
+import App from './App.vue'
+import router from './router'
+
+import { createApp } from 'vue'
+import '@mdi/font/css/materialdesignicons.css'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const app = createApp(App)
+
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+      defaultSet: 'mdi', 
+    },
+    theme: {
+
+    },
+})
+
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
---
+++ resources/js/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+<v-card>
+  <v-card-content>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus a fugiat placeat accusantium at deserunt dicta maxime excepturi assumenda. Nemo, reprehenderit! Quam ipsam reprehenderit provident aut harum atque ratione consequuntur hic. A, totam odio. Maxime aliquid qui animi odio unde nostrum tempore dolorum, dolore earum sit laudantium, dolores voluptas consequatur, nobis aut eligendi nulla molestias?
+  </v-card-content>
+</v-card>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
