28562aafa115e31218d6f8504c351c1de025af7d
kaderabdul79 <kaderabdul7980@gmail.com>
2023-10-24 06:19:51 +0600

    send request login endpoint and receive token

--- resources/js/views/Login.vue
+++ resources/js/views/Login.vue
@@ -2,7 +2,7 @@
     <div id="login" class="h-100 bg-blue-lighten-1 pa-3 rounded">
         <v-sheet width="500" class="mx-auto">
             <div class="text-h3 bg-blue-darken-2 text-center py-4">Login</div>
-            <v-form class="pa-3" fast-fail>
+            <v-form class="pa-3" fast-fail @submit.prevent="handleLogin">
                 <v-text-field
                     v-model="email"
                     label="email"
@@ -20,7 +20,28 @@
 </template>
 
 <script setup>
-
+import {ref,onMounted} from 'vue'
+import axios from 'axios'
+axios.defaults.baseURL = "http://127.0.0.1:8000/api/"
+const email = ref("kader@gmail.com")
+const password = ref("11111111")
+function handleLogin(){
+    axios.get('sanctum/csrf-cookie').then(response => {
+    axios.post('login', {
+      email: email.value,
+      password: password.value,
+    }).then(response => {
+      console.log(response.data);
+    }).catch(error => {
+      console.error(error);
+    });
+  });
+}
+onMounted(()=>{
+    axios.get('sanctum/csrf-cookie').then(response => {
+        console.log(response);
+    });
+})
 </script>
 
 <style scoped>
