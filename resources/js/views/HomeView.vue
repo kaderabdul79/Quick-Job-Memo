f8cdddd807f09da2b9415f98c2919f0722ae5ccf
kaderabdul79 <kaderabdul7980@gmail.com>
2023-10-25 06:19:51 +0600

    if token available , retrive user information

--- resources/js/views/HomeView.vue
+++ resources/js/views/HomeView.vue
@@ -1,6 +1,16 @@
 <template>
 <v-card>
 <SiteHeader />
+<div v-if="user">
+      <p>User Information:</p>
+      <ul>
+        <li>Email: {{ user.email }}</li>
+        <!-- Add more user information as needed -->
+      </ul>
+    </div>
+    <div v-else>
+      <p>No user information available.</p>
+    </div>
 <Sitecarousal />
 <SiteFooter />
 </v-card>
@@ -11,6 +21,30 @@ import SiteFooter from '../components/SiteFooter.vue';
 import SiteHeader from '../components/SiteHeader.vue';
 import Sitecarousal from '../components/sitecarousal.vue';
 
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+axios.defaults.baseURL = "http://127.0.0.1:8000/api/";
+
+const user = ref(null);
+
+onMounted(async () => {
+  try {
+    // retrive token from local storage
+    const storedToken = localStorage.getItem('token');
+
+    if (storedToken) {
+      // Set the stored token in the headers
+      axios.defaults.headers.common['Authorization'] = `Bearer ${storedToken}`;
+
+      // Retrieve user information
+      const response = await axios.get('user');
+      console.log(response);
+      user.value = response.data.data;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 </script>
 
